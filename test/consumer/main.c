dddd4f67c566c0ab06f078990df32fe0f05a0702
Rob Loach <robloach@gmail.com>
2026-07-10 11:23:12 -0400

    Delete test/consumer/main.c

--- test/consumer/main.c
+++
@@ -1,9 +0,0 @@
-#define SDL_LIBRETRO_IMPLEMENTATION
-#include <SDL_libretro.h>
-
-int main(void) {
-    SDL_Libretro* lr = SDL_Libretro_Create();
-    if (!lr) return 1;
-    SDL_Libretro_Destroy(lr);
-    return 0;
-}
